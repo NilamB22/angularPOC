e515bac14f12d6c1c16cccae4252a2fdc9d70152
Nilam Padval <nilamp@citiustech.com>
2019-05-22 16:11:38 +0530

    deployment isse :1

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class DataService {
-  ROOT_URL = '../assets/data.json';
+  ROOT_URL = './assets/data.json';
 
   constructor(private http: HttpClient) {}
 
